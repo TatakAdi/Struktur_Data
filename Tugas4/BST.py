727316cd431b8deed04166fcc7626ec05080baa0
TatakAdi <tatakadisubagyo@gmail.com>
2023-10-08 21:57:07 +0800

    Menambahkan fungsi remove

--- Tugas4/BST.py
+++ Tugas4/BST.py
@@ -5,16 +5,22 @@ class Node:
         self.right = None
 
 def insert(node,root):
+    if isinstance(root, int):
+        if node is None:
+            return Node(root)
+        if root < node.root:
+            node.left = insert(node.left, root)
+        elif root > node.root:
+            node.right = insert(node.right, root)
+        return node
+    else:
+        raise IndexError ("Masukkan data bertipe interger")
 
-    if node is None:
-        return Node(root)
-
-    if root < node.root:
-        node.left = insert(node.left, root)
-    elif root > node.root:
-        node.right = insert(node.right, root)
-
-    return node
+def Preorder(node):
+    if node:
+        print(node.root,end=" ")
+        Preorder(node.left)
+        Preorder(node.right)
 
 def inorder(node):
     if node:
@@ -22,7 +28,34 @@ def inorder(node):
         print(node.root,end=" ")
         inorder(node.right)
 
-r = Node(50)
+def Postorder(node):
+    if node:
+        Postorder(node.left)
+        Postorder(node.right)
+        print(node.root,end=" ")
+    else : 
+        return "Ini bukan BST"
+
+# def remove(node,root):
+#     if node is None:
+#         return root
+#     if root < node.root:
+#         node.left = remove(node.left,root)
+#         return root
+#     elif root > node.root:
+#         node.right = remove(node.right, root)
+
+
+def order(root):
+    print("BST saat Preorder : ")
+    Preorder(root)
+    print("\nBST saat inorder : ")
+    inorder(root)
+    print("\nBST saat Postorder : ")
+    Postorder(root)
+
+r = None
+r = insert(r,50)
 r = insert(r,30)
 r = insert(r,20)
 r = insert(r,40)
@@ -30,5 +63,4 @@ r = insert(r,70)
 r = insert(r,60)
 r = insert(r,80)
 
-inorder(r)
-
+order(r)
